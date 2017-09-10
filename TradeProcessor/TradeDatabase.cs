6b385a2e04746b4cb8eafa2da1a6eb3ac9aee7ce
Stefan Mehnert <s.mehnert@polipol.de>
2017-09-10 13:19:02 +0200

    Refactored the database access coding

--- TradeProcessor/TradeDatabase.cs
+++ TradeProcessor/TradeDatabase.cs
@@ -13,29 +13,32 @@ namespace TradeProcessor.ConsoleApp
             _sqlConnection = sqlConnection;
         }
 
-        public void InsertTradeRecords(IEnumerable<TradeRecord> tradeRecords)
+        public void InsertTradeRecords(List<TradeRecord> tradeRecords)
         {
             _sqlConnection.Open();
+
             using (var transaction = _sqlConnection.BeginTransaction())
             {
-                foreach (var trade in tradeRecords)
-                {
-                    var command = _sqlConnection.CreateCommand();
-                    command.Transaction = transaction;
-                    command.CommandType = System.Data.CommandType.StoredProcedure;
-                    command.CommandText = "dbo.insert_trade";
-                    command.Parameters.AddWithValue("@sourceCurrency", trade.SourceCurrency);
-                    command.Parameters.AddWithValue("@destinationCurrency", trade.DestinationCurrency);
-                    command.Parameters.AddWithValue("@lots", trade.Lots);
-                    command.Parameters.AddWithValue("@price", trade.Price);
-
-                    command.ExecuteNonQuery();
-                }
+                tradeRecords.ForEach(trade => ExecInsertTrade(transaction, trade));
 
                 transaction.Commit();
             }
-            _sqlConnection.Close();
 
+            _sqlConnection.Close();
+        }
+
+        private void ExecInsertTrade(SqlTransaction transaction, TradeRecord trade)
+        {
+            var command = _sqlConnection.CreateCommand();
+            command.Transaction = transaction;
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.CommandText = "dbo.insert_trade";
+            command.Parameters.AddWithValue("@sourceCurrency", trade.SourceCurrency);
+            command.Parameters.AddWithValue("@destinationCurrency", trade.DestinationCurrency);
+            command.Parameters.AddWithValue("@lots", trade.Lots);
+            command.Parameters.AddWithValue("@price", trade.Price);
+
+            command.ExecuteNonQuery();
         }
 
         public void Dispose()
